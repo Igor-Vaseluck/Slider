78eade1c430c7ae3f3f69f885dc5672a0b89fa95
Igor-Vaseluck <141558736+Igor-Vaseluck@users.noreply.github.com>
2023-08-13 00:33:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -56,3 +56,30 @@ dot3.addEventListener('click', function(){
     }
 )
 
+
+document.addEventListener('keydown', function(event) {
+    if(event.code == 'ArrowRight'){
+        if(index<array.length){
+            img.src=array[index]
+            index+=1
+        } else{
+            index=0
+            img.src=array[index]
+            index+=1
+        }
+        setTimeout(()=>dot2.checked = true, 500)
+    }
+})
+document.addEventListener('keydown', function(event) {
+    if(event.code == 'ArrowLeft'){
+        if(index>0){
+            img.src=array[index]
+            index-=1
+        } else{
+            index=array.length-1
+            img.src=array[index]
+            index-=1
+        }
+        setTimeout(()=>dot2.checked = true, 500)
+    }
+})
